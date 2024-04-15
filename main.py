979aae7dff6dffcc7dfcc427b2a7944678497168
prokoposgiannis <giannisprokopos@gmail.com>
2024-04-16 00:57:28 +0300

    more suitable layout and dummy buttons

--- main.py
+++ main.py
@@ -6,35 +6,34 @@ class MainWindow(tk.Tk):
         self.title("Ηλεκτρονική δανειστική βιβλιοθήκη")
         self.geometry("1000x800")
 
-        # Sidebar
-        self.sidebar_frame = tk.Frame(self, width=200)
-        self.sidebar_frame.pack(side="left", fill="both")
-        self.sidebar_frame.pack_propagate(0)  # Prevent frame from resizing
-
-        self.sidebar_border_frame = tk.Frame(self.sidebar_frame, width=2, bg="black")
-        self.sidebar_border_frame.pack(side="right", fill="y")
-
         # Main frame
         self.main_frame = tk.Frame(self)
-        self.main_frame.pack(side="right", fill="both", expand=True)
+        self.main_frame.pack(fill="both", expand=True)
 
         # Widgets
-        # Widgets for the sidebar
-        self.sidebar_label = tk.Label(self.sidebar_frame, text="Εργαλεία", font=("Helvetica", 16))
-        self.sidebar_label.pack(pady=20)
-
-        self.sidebar_button = tk.Button(self.sidebar_frame, text="Button1", command=self.button_clicked)
-        self.sidebar_button.pack(padx=10, pady=10, anchor="w")
-        self.sidebar_button = tk.Button(self.sidebar_frame, text="Button2", command=self.button_clicked)
-        self.sidebar_button.pack(padx=10, pady=10, anchor="w")
-
         # Widgets for the main page
         self.homepage_label = tk.Label(self.main_frame, text="Αρχική σελίδα", font=("Helvetica", 16))
         self.homepage_label.pack(pady=20)
 
-    # Functions
-    def button_clicked(self):
-        print("Clicked")
+        # Books button
+        self.books_canvas = tk.Canvas(self.main_frame, width=400, height=400, bg="white")
+        self.books_canvas.pack(side="left", padx=50)
+        self.books_rectangle = self.books_canvas.create_rectangle(0, 0, 400, 400, fill="#D7D0C2")
+        self.books_text = self.books_canvas.create_text(200, 200, text="Βιβλία", fill="black", font=("Helvetica", 40))
+        self.books_canvas.tag_bind(self.books_rectangle, "<Button-1>", self.onClickBooks)
+
+        # Users button
+        self.users_canvas = tk.Canvas(self.main_frame, width=400, height=400, bg="white")
+        self.users_canvas.pack(side="right", padx=50)
+        self.users_rectangle = self.users_canvas.create_rectangle(0, 0, 400, 400, fill="#D7D0C2")
+        self.users_text = self.users_canvas.create_text(200, 200, text="Χρήστες", fill="black", font=("Helvetica", 40))
+        self.users_canvas.tag_bind(self.users_rectangle, "<Button-1>", self.onClickUsers)
+
+    def onClickBooks(self, event):
+        print("Βιβλία")
+
+    def onClickUsers(self, event):
+        print("Χρήστες")
 
 if __name__ == "__main__":
     app = MainWindow()
