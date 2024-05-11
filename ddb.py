03c6ba6602ae4011e9339d0f9d978bd51e30b1c5
prokoposgiannis <giannisprokopos@gmail.com>
2024-05-11 16:03:07 +0300

    new buttons in ddb.py

---
+++ ddb.py
@@ -0,0 +1,37 @@
+#Εισαγωγή βιβλιοθηκών
+import tkinter as tk
+
+#Εισαγωγή Σελίδων
+from src.pages.HomePage import HomePage
+from src.pages.BooksPage import BooksPage
+from src.pages.UsersPage import UsersPage
+
+#Ορισμός κλάσης βασικού παραθύρου
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Διαχείρισης Δανειστικής Βιβλιοθήκης") #Τίτλος παραθύρου
+        self.geometry("1000x800") #Τίτλος παραθύρου
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (HomePage, BooksPage, UsersPage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame(HomePage)
+    
+    def show_frame(self, context):
+        frame = self.frames[context]
+        frame.tkraise()
+
+app = MainWindow()
+app.mainloop()
+
+       
--- src/pages/HomePage.py
+++ src/pages/HomePage.py
@@ -5,26 +5,57 @@ import os
 class HomePage(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label = tk.Label(self, text="Αρχική σελίδα", font=("Helvetica", 16))
-		label.pack(padx=20, pady=20)
+		header = tk.Label(self, text="Διαχείρισης Δανειστικής Βιβλιοθήκης", font=("Helvetica", 24))
+		header.grid(row=0, column=0,padx=20, columnspan=3, sticky="nsew", pady=(20, 50))
+
 		script_dir = os.path.dirname(os.path.abspath(__file__))
 		images_dir = os.path.join(script_dir, "../images/")
 
 		from .BooksPage import BooksPage
 		from .UsersPage import UsersPage
-		
-        # Books Button
+		from .BorrowingsPage import BorrowingsPage
+		from .StatisticsPage import StatisticsPage
+	
+        # Κουμπί σελίδας βιβλίων
 		books_image_path = os.path.join(images_dir, "books.png")
 		rawBooksImage = tk.PhotoImage(file=books_image_path)
 		resizedBooksImage = rawBooksImage.subsample(2, 2)  
 		booksButton = tk.Button(self, image=resizedBooksImage, command=lambda: controller.show_frame(BooksPage))
 		booksButton.image = resizedBooksImage  
-		booksButton.pack(side="left", padx=100)
+		booksButton.grid(row=1, column=0, padx=(150, 50))
+
+		booksLabel = tk.Label(self, text="Διαχείριση βιβλίων", font=("Helvetica", 16))
+		booksLabel.grid(row=2, column=0,padx=(150,50), sticky="nsew", pady=(5, 20))
 		
-        # Users Button
+        # Κουμπί σελίδας χρηστών
 		users_image_path = os.path.join(images_dir, "users.png")
 		raw_users_image = tk.PhotoImage(file=users_image_path)
 		resizedUsersImage = raw_users_image.subsample(4, 4)  
 		usersButton = tk.Button(self, image=resizedUsersImage, command=lambda: controller.show_frame(UsersPage))
 		usersButton.image = resizedUsersImage  
-		usersButton.pack(side="right", padx=100)
+		usersButton.grid(row=1, column=1, padx=(50, 100))
+
+		booksLabel = tk.Label(self, text="Διαχείριση χρηστών", font=("Helvetica", 16))
+		booksLabel.grid(row=2, column=1,padx=(50, 100), sticky="nsew", pady=(5, 20))
+
+        # Κουμπί σελίδας δανεισμών
+		books_image_path = os.path.join(images_dir, "borrowings.png")
+		rawBooksImage = tk.PhotoImage(file=books_image_path)
+		resizedBooksImage = rawBooksImage.subsample(2, 2)  
+		booksButton = tk.Button(self, image=resizedBooksImage, command=lambda: controller.show_frame(BorrowingsPage))
+		booksButton.image = resizedBooksImage  
+		booksButton.grid(row=3, column=0, padx=(150, 50))
+
+		booksLabel = tk.Label(self, text="Διαχείριση δανεισμών", font=("Helvetica", 16))
+		booksLabel.grid(row=4, column=0,padx=(150, 50), sticky="nsew", pady=(5, 20))
+		
+        # Κουμπί σελίδας στατιστικών
+		users_image_path = os.path.join(images_dir, "statictics.png")
+		raw_users_image = tk.PhotoImage(file=users_image_path)
+		resizedUsersImage = raw_users_image.subsample(4, 4)  
+		usersButton = tk.Button(self, image=resizedUsersImage, command=lambda: controller.show_frame(StatisticsPage))
+		usersButton.image = resizedUsersImage  
+		usersButton.grid(row=3, column=1, padx=(50, 100))
+
+		booksLabel = tk.Label(self, text="Στατιστικά", font=("Helvetica", 16))
+		booksLabel.grid(row=4, column=1,padx=(50, 100), sticky="nsew", pady=(5, 20))
