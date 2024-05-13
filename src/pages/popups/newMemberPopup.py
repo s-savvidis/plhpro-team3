aee224e2dd520c8ff61ee0f663b48fbe5c9a1382
PrassasOrpheas <std164201@ac.eap.gr>
2024-05-13 18:11:55 +0300

    fixed newMemberPopup and updateMemberPopup

--- src/pages/popups/newMemberPopup.py
+++ src/pages/popups/newMemberPopup.py
@@ -27,7 +27,7 @@ def newMemberPopup(self):
 	age=tk.IntVar(value=0)
 	entry_field2 = tk.Spinbox(popup, from_= 1, to = 99, width=4, increment=1,
 	textvariable=age)
-	entry_field2.grid(row=5, column=1, sticky="ew", padx=10)
+	entry_field2.grid(row=1, column=1, sticky="ew", padx=10)
 
 	tk.Label(popup, text="Επάγγελμα:").grid(row=2, column=0, sticky="w", padx=10)
 	entry_field3 = tk.Entry(popup)
@@ -43,18 +43,18 @@ def newMemberPopup(self):
 
 	tk.Label(popup, text="Φύλο:").grid(row=5, column=0, sticky="w", padx=10)
 	entry_field6 = tk.OptionMenu(popup, defaultGender, *genderOptions)
-	entry_field6.grid(row=2, column=1, sticky="ew", padx=10)
+	entry_field6.grid(row=5, column=1, sticky="ew", padx=10)
 
-	insert_button = tk.Button(popup, text="Εισαγωγή", command= lambda: addBook())
+	insert_button = tk.Button(popup, text="Εισαγωγή", command= lambda: addMember())
 	insert_button.grid(row=6, column=0, pady=10, padx=10,sticky="w")
 	
 	close_button = tk.Button(popup, text="Κλείσιμο", command=popup.destroy)
 	close_button.grid(row=6, column=1, pady=10, sticky="w")
 
-	def addBook():
+	def addMember():
 		memberDetails = {
 			'name': entry_field1.get(),
-			'age': entry_field2(),
+			'age': entry_field2.get(),
 			'occupation': entry_field3.get(),
 			'tel': entry_field4.get(),
 			'email': entry_field5.get(),
@@ -71,7 +71,5 @@ def newMemberPopup(self):
 			self.memberIDLabel.configure(text=f"-")
 			popup.destroy()
 		else:
-			#εκκρεμεί λαβελ.
-			entry_field1.insert(0, "Απαραίτητο πεδίο")
-			entry_field2.insert(0, "Απαραίτητο πεδίο")
-			entry_field4.insert(0, "Απαραίτητο πεδίο")
+			error_label = tk.Label(popup, text="Συμπληρώστε όλα τα απαραίτητα πεδία με *", fg="red")
+			error_label.place(x=40, y=1)
--- src/pages/popups/updateMemberPopup.py
+++ src/pages/popups/updateMemberPopup.py
@@ -6,36 +6,60 @@ def updateMemberPopup(self, memberDetails):
 		popup = tk.Toplevel()
 		popup.title("Ενημέρωση μέλους")
 
+		genderOptions = [
+		"f",
+		"m",
+		"o",
+		] 
+
+		defaultGender = tk.StringVar()
+		defaultGender.set(genderOptions[0]) # default value
+  
 		XYPoints = centerizePopup(self, popup)
 		popup.geometry(f"+{XYPoints["x"]}+{XYPoints["y"]}")
-
-		tk.Label(popup, text="Φύλο:").grid(row=0, column=0, sticky="w", padx=10)
-		gender=tk.IntVar(value=memberDetails["gender"])
-		entry_field6 = tk.Spinbox(popup, from_= 1, to = 99, increment=1,
-    	textvariable=gender)
-		entry_field6.grid(row=0, column=1, sticky="ew", padx=10)
-
+  
+		tk.Label(popup, text="Όνοματεπώνυμο:").grid(row=0, column=0, sticky="w", pady=(10,0), padx=10)  
+		entry_field1 = tk.Entry(popup)
+		entry_field1.grid(row=0, column=1, sticky="ew", pady=(10,0), padx=10)
+  
 		tk.Label(popup, text="Ηλικία:").grid(row=1, column=0, sticky="w", padx=10)
 		age=tk.IntVar(value=memberDetails["age"])
 		entry_field2 = tk.Spinbox(popup, from_= 1, to = 99, width=4, increment=1,
     	textvariable=age)
-		entry_field2.grid(row=1, column=1, sticky="ew", padx=10)
+		entry_field2.grid(row=1, column=1, sticky="ew", padx=10)  
 
-		insert_button = tk.Button(popup, text="Ενημέρωση", command= lambda: updateMember(entry_field2.get(), entry_field6.get()))
-		insert_button.grid(row=2, column=0, pady=10, padx=10,sticky="w")
+		tk.Label(popup, text="Επάγγελμα:").grid(row=2, column=0, sticky="w", padx=10)
+		entry_field3 = tk.Entry(popup)
+		entry_field3.grid(row=2, column=1, sticky="ew", padx=10)
+
+		tk.Label(popup, text="Τηλέφωνο:").grid(row=3, column=0, sticky="w", padx=10)
+		entry_field4 = tk.Entry(popup)
+		entry_field4.grid(row=3, column=1, sticky="ew", padx=10)
+
+		tk.Label(popup, text="email:").grid(row=4, column=0, sticky="w", padx=10)
+		entry_field5 = tk.Entry(popup)
+		entry_field5.grid(row=4, column=1, sticky="ew", padx=10)
+
+		tk.Label(popup, text="Φύλο:").grid(row=5, column=0, sticky="w", padx=10)
+		entry_field6 = tk.OptionMenu(popup, defaultGender, *genderOptions)
+		entry_field6.grid(row=5, column=1, sticky="ew", padx=10)
+
+
+
+		insert_button = tk.Button(popup, text="Ενημέρωση", command= lambda: updateMember(entry_field1.get(), entry_field2.get(), entry_field3.get(), entry_field4.get(), entry_field5.get(), entry_field6.get()))
+		insert_button.grid(row=6, column=0, pady=10, padx=10,sticky="w")
 		
 		close_button = tk.Button(popup, text="Κλείσιμο", command=popup.destroy)
-		close_button.grid(row=2, column=1, pady=10, sticky="w")
+		close_button.grid(row=6, column=1, pady=10, sticky="w")
 
-		def updateMember(age, gender):
+		def updateMember():
 			memberDetails = {
-				'name': self.entry_field1.get(),
-                'age': age,
-                'occupation': self.entry_field2.get(),
-                'tel': self.entry_field3.get(),
-                'email': self.entry_field4.get(),
-                'gender': gender,
-				'member_id': self.selectedMember["member_id"]
+			'name': entry_field1.get(),
+			'age': entry_field2(),
+			'occupation': entry_field3.get(),
+			'tel': entry_field4.get(),
+			'email': entry_field5.get(),
+			'gender': defaultGender.get()
 			}
 			dtb.update_member(self.db, memberDetails)
 			showMembers(self, self.db, "")
