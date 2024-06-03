69c93b71aa7f7f635053a2ce8a0d1856e0cb080f
PrassasOrpheas <std164201@ac.eap.gr>
2024-06-03 16:58:31 +0300

    added members and borrowings pages

--- src/database/library_member_manage.py
+++ src/database/library_member_manage.py
@@ -54,15 +54,16 @@ class library_members():
     def insert_member(self, memberDetails):
         '''Εισαγωγή μέλους στη βάση'''
 
-        sql = ''' INSERT INTO members (name, age, occupation, tel, email) VALUES (?,?,?,?,?) '''
+        sql = ''' INSERT INTO members (name, age, occupation, tel, email, gender) VALUES (?,?,?,?,?,?) '''
         cur = self.conn.cursor()
-        dbConn = self.conn
+        dbConn = self.conn            
         try:
             cur.execute(sql, (memberDetails['full_name'],
                               memberDetails['age'],
                               memberDetails['occupation'],
                               memberDetails['telephone_number'],
-                              memberDetails['email']
+                              memberDetails['email'],
+                              memberDetails['gender']
                               )
                         )
             logging.info("Εισαγωγή νέου μέλους στη βάση. {}".format(memberDetails['full_name']))
@@ -74,9 +75,10 @@ class library_members():
         except Exception as e:
             logging.error("Πρόβλημα εισαγωγής μέλους {} στη βάση. Πρόβλημα: {}".format(memberDetails['full_name'], e))
             return False
+        
     def update_member(self, memberDetails):
         '''Επικαιροποίηση στοιχείων μέλους'''
-        sql = ''' UPDATE members SET (name, age, occupation, tel, email) VALUES (?,?,?,?,?) WHERE member_id=? '''
+        sql = '''UPDATE members SET name=?, age=?, occupation=?, tel=?, email=?, gender=? WHERE member_id=?'''
         cur = self.conn.cursor()
         dbConn = self.conn
         try:
@@ -85,6 +87,7 @@ class library_members():
                               memberDetails['occupation'],
                               memberDetails['telephone_number'],
                               memberDetails['email'],
+                              memberDetails['gender'],
                               memberDetails['member_id']
                               )
                         )
@@ -106,9 +109,72 @@ class library_members():
             dbConn.commit()
             return True
         except Exception as e:
-            loggin.error("Αποτυχία διαγραφής μέλους με κωδικό {}. Λάθος: {}".format(memberId, e))
+            logging.error("Αποτυχία διαγραφής μέλους με κωδικό {}. Λάθος: {}".format(memberId, e))
             return False
 
+    def recommendations(self, member_id):
+        ''' Πρόταση δανεισμού μέσω Ratings '''
+        cur = self.conn.cursor()
+    
+        # Βρίσκουμε το ιστορικό δανεισμών του μέλους στο οποίο θέλουμε να προτείνουμε βιβλία
+        cur.execute('''SELECT book_id FROM borrowings WHERE member_id = ?''', (member_id,))
+        borrowing_history = cur.fetchall()
+    
+        #Αν δεν έχει δανειστεί βιβλία τότε λόγο μη επαρκούς ιστορικού του προτείνουμε τα βιβλία με τις καλύτερες 
+        # συνολικές κριτικές από πέντε διαφορετικές κατηγορίες
+        if len(borrowing_history) == 0:
+            cur.execute('''SELECT books.book_id, books.title
+                    FROM books 
+                    LEFT JOIN borrowings ON books.book_id = borrowings.book_id 
+                    WHERE books.category IN (SELECT DISTINCT category FROM books) 
+                    GROUP BY books.book_id 
+                    ORDER BY AVG(borrowings.rating) DESC 
+                    LIMIT 5''')
+            suggestions = cur.fetchall()
+        else:
+            #Αν έχει δανειστεί τουλάχιστον ένα βιβλία, βρίσκουμε τα βιβλία που του άρεσαν περισσότερο (μέχρι 10 για να αποφύγουμε χαμηλές βαθμολογίες)
+            cur.execute('''SELECT book_id FROM borrowings 
+                        WHERE member_id = ? 
+                        ORDER BY rating DESC 
+                        LIMIT 10''', (member_id,))
+            top_rated = cur.fetchall()
+        
+        
+        # Βρίσκουμε το καλύερο βιβλίο που δεν έχει δανειστεί, βάση της μέσης βαθμολογίας του, για κάθε ξεχωριστή κατηγορία βιβλίου που υπάρχει στο ιστορικό του.
+            suggested_categories = []
+            suggestions = []
+            for book_id, in top_rated:
+                cur.execute('''SELECT category FROM books WHERE book_id = ?''', (book_id,))
+                category = cur.fetchone()[0]
+                if category not in suggested_categories:
+                    suggested_categories.append(category)
+                    cur.execute('''SELECT books.book_id, books.title 
+                               FROM books 
+                               LEFT JOIN borrowings ON books.book_id = borrowings.book_id AND borrowings.member_id = ?
+                               WHERE books.category = ?
+                               AND borrowings.member_id IS NULL
+                               GROUP BY books.book_id, books.title 
+                               ORDER BY AVG(borrowings.rating) DESC
+                               LIMIT 1''', (member_id, category))
+                    suggested_book = cur.fetchone()
+                    if suggested_book:
+                        suggestions.append(suggested_book)
+
+            # Αν υπάρχουν λιγότερες από 5 προτάσεις, συμπληρώνουμε με τα καλύτερα βάση βαθμολογίας χρηστών βιβλία, από άλλες κατηγορίες
+            while len(suggestions) < 5:
+                cur.execute('''SELECT books.book_id, books.title
+                           FROM books 
+                           LEFT JOIN borrowings ON books.book_id = borrowings.book_id 
+                           WHERE books.category NOT IN ({})
+                           GROUP BY books.book_id 
+                           ORDER BY AVG(borrowings.rating) DESC 
+                           LIMIT {}'''.format(','.join(['?']*len(suggested_categories)), (5 - len(suggestions))), list(suggested_categories))
+                remaining_suggestions = cur.fetchall()
+                suggestions.extend(remaining_suggestions)
+                
+    
+        return suggestions
+        
 #######################################
 if __name__ == '__main__':
     import argparse
@@ -136,6 +202,7 @@ if __name__ == '__main__':
     newMembr['occupation'] = input("Επάγγελμα: ")
     newMembr['telephone_number'] = input("Τηλέφωνο: ")
     newMembr['email'] = input("Email: ")
+    newMembr['gender'] = input("gender: ")
     
     insertResult = libMembers.insert_member(newMembr)
     if not insertResult:
